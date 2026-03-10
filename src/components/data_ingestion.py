997df4592787a1b1a2bb789455abc82b43071c07
Yiheng Xie <xieyiheng00@gmail.com>
2026-03-10 14:01:44 -0500

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 
 @dataclass
@@ -21,7 +23,7 @@ class DataIngestion:
         logging.info("Enter the data ingestion method or component")
 
         try:
-            df=pd.read_csv('notebook\data.csv')
+            df=pd.read_csv('notebook/data.csv')
             logging.info('Read the dateset as dataframe')
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             logging.info("train test split initiated")
@@ -30,7 +32,6 @@ class DataIngestion:
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
             logging.info("Ingestion of the data is completed")
             return (
                 self.ingestion_config.train_data_path,
@@ -42,4 +43,6 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
